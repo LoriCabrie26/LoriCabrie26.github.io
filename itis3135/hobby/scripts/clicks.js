0d6f34b6c4542b12ba8a2c5a87638044ab55a854
LoriCabrie26 <loricabrie@gmail.com>
2025-03-17 20:14:17 -0400

    hobby

---
+++ itis3135/hobby/scripts/clicks.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const collapsibles = document.querySelectorAll(".collapsible-header");
+
+    collapsibles.forEach(header => {
+        header.addEventListener("click", function () {
+            this.parentElement.classList.toggle("active");
+        });
+    });
+});
